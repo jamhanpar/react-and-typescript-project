e305b899e48c6dabf9d1b23ecb21de6f8c262b4f
James Park <james.hansung.park@gmail.com>
2021-12-29 14:03:27 -0500

    added various prop type notes

--- examples/02-nametag-with-props/src/Application.tsx
+++ examples/02-nametag-with-props/src/Application.tsx
@@ -1,6 +1,58 @@
+import React from "react";
+
 type NameTagProps = {
   name: string;
+  // count: number;
+  // enabled: boolean;
+  // items: string[];
+  // status: 'loading' | 'error' | 'success';
+  // anObject: {};
+  // item: {
+  //   id: string;
+  //   title: string;
+  // }
+  // items: {
+  //   id: string;
+  //   title: string;
+  // }[];
 };
+
+// type Item = {
+//   id: string;
+//   title: string;
+// }
+
+// type ItemPropTypes = {
+//   item: Item;
+//   items: Item[];
+// }
+
+// type ItemHash = {
+//   [key: string]: Item;
+// };
+
+// type Dictionary = {
+//   [key: number]: string;
+// }
+
+// type Functions = {
+//   onHover: () => void;
+//   onChange: (id: number) => void;
+//   onClick(event: React.MouseEvent<HTMLButtonElement>): void;
+// }
+
+// const add = (a: number, b: number): number => {
+//   return a + b;
+// };
+
+// function subtract(a: number, b: number): number {
+//   return a - b;
+// }
+
+// type OptionalProps = {
+//   requiredProp: boolean;
+//   optionalProp?: string;
+// }
 
 const NameTag = ({ name }: NameTagProps) => {
   return (
