55d4865946464ec0ecb7e1b21d9a23ba109dfc21
James Park <james.hansung.park@gmail.com>
2021-12-29 15:31:49 -0500

    added .env file to avoid preflight check

--- examples/06-avengers-quiz/src/Application.tsx
+++ examples/06-avengers-quiz/src/Application.tsx
@@ -1,4 +1,5 @@
 import { questions } from './questions';
+import { useState } from 'react';
 
 type QuestionProps = {
   question: string;
@@ -6,11 +7,13 @@ type QuestionProps = {
 };
 
 const Question = ({ question, answer }: QuestionProps) => {
+  const [isHidden, toggleHidden] = useState(true);
+  
   return (
     <article className="question">
       <header>{question}</header>
       <p className="answer">
-        <span className="blurred">{answer}</span>
+        <span className={`${isHidden ? 'blurred' : 'visible'}`}>{answer}</span>
       </p>
       <footer>
         <button>Toggle Answer</button>
